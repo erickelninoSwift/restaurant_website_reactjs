8c3b229470b58a82e91f059f1cbb70d225c05473
Erick El nino <erick.tshimbombo@yahoo.com>
2024-01-10 12:50:16 +0200

    done with layout

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import { Navbar } from "./components/Navbar";
 import Hero from "./components/Hero";
 import HeadlinesCard from "./components/HeadlinesCard";
 import Food from "./components/Food";
+import Category from "./components/Category";
 
 function App() {
   return (
@@ -11,6 +12,7 @@ function App() {
       <Hero />
       <HeadlinesCard />
       <Food />
+      <Category />
     </div>
   );
 }
